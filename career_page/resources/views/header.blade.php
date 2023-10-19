06a0d0f5205f7f0ff81335820b2b5af479a39d49
aartighorpade21 <aarti.ghorpade@phntechnology.com>
2023-10-19 13:03:11 +0530

    header_section

--- career_page/resources/views/header.blade.php
+++ career_page/resources/views/header.blade.php
@@ -20,32 +20,33 @@
     <section class="Header_section">
         <div class="container-fluid">
             <div class="container">
-            <div className="logo">
+            <div class="logo">
             <img src="{{ asset('img/phn_tech_logo.png') }}" alt="PHN_logo" />
           </div>
-          <div className="header">
-            <div className="row">
+
+          <div class="header">
+            <div class="row">
               <div
-                className="col-sm-12 col-md-12 col-lg-6 col-12 header_details_div"
+                class="col-sm-12 col-md-12 col-lg-6 col-12 header_details_div"
               >
-                <p className="career_title">#JOB</p>
-                <p className="header_title">
+                <p class="career_title">#Career</p>
+                <p class="header_title">
                   Join our Innovative Team and Shape the Future with us!
                 </p>
-                <p className="header_subtitle">
+                <p class="header_subtitle">
                   Explore exciting career opportunities at PHN Technology
                 </p>
 
-                <button className="apply_btn btn">Apply Now</button>
+                <button class="apply_btn btn">Apply Now</button>
               </div>
 
               <div
-                className="col-sm-12 col-md-12 col-lg-6 col-12 header_img_div"
+                class="col-sm-12 col-md-12 col-lg-6 col-12 header_img_div"
               >
                 <img
                   src="{{ asset('img/header_img.webp') }}"
                   alt="header_img"
-                  className="header_img img-fluid"
+                  class="header_img img-fluid"
                 />
               </div>
             </div>
